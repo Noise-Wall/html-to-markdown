edeb1edaff42b0e70c927a2efddb2aaeab6c4b82
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2024-02-08 11:57:49 -0700

    Better naming and renaming features
    Added capitalization; added renaming of index files to its
    parent folder's name; added renaming capabilities to .md files

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 A simple, personal program to parse HTML files to Markdown. For my Obsidian vaults.
 
+If the filename is an index page, the program renames the file to that of its directory. If the file is of type `.md`, it skips the conversion part and renames if needed.
+
 Utilizes ![turndown](https://github.com/mixmark-io/turndown). Not much else.
+
+### Usage
+
+Requires *Node.js* (tested with version 21.6.1)
+
+```bash
+$ /path/to/project/run.sh $(file)
+```
+
+Paired with bash command `find`:
+
+```bash
+$ find ./ -type f -name $(file) -exec sh -c /path/to/project/run.sh '{}' \; 
+```
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fs = require("fs").promises;
-const { resolve, parse } = require("path");
+const { resolve, parse, basename } = require("path");
 
 const turndown = require("turndown");
 
@@ -10,13 +10,21 @@ const service = new turndown({
   // preformattedCode: true
 });
 
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 async function readFile(path) {
   const file = await fs.readFile(path, "utf8");
   return file;
 }
 
 async function toFile(title, string) {
-  const file = await fs.writeFile(title, string);
+  await fs.writeFile(title, string);
+}
+
+async function renameFile(oldPath, newPath) {
+  await fs.rename(oldPath, newPath);
 }
 
 async function main() {
@@ -25,19 +33,28 @@ async function main() {
     return;
   }
   const parsedFile = parse(resolve(process.argv[2]));
-  if (parsedFile.ext !== ".html") {
-    console.log(`Error: file must be of type '.html.'`);
+  if (parsedFile.ext !== ".html" && parsedFile.ext !== ".md") {
+    console.log(`Error: file must be of type '.html' or '.md'.`);
     return;
   }
 
-  // console.log(parsedFile.dir);
-
   const file = await readFile(process.argv[2]);
-  const md = service.turndown(file);
+  const newFileName =
+    parsedFile.name === "index"
+      ? `${capitalize(basename(parsedFile.dir))}`
+      : capitalize(parsedFile.name);
 
-  await toFile(`${parsedFile.dir}/${parsedFile.name}.md`, md)
-    .then(() => console.log("Success!"))
-    .catch((e) => console.log(`Error: ${e.message}`));
+  if (parsedFile.ext == ".html") {
+    const md = service.turndown(file);
+
+    await toFile(`${parsedFile.dir}/${newFileName}.md`, md)
+      .then(() => console.log("HTML converted successfully!"))
+      .catch((e) => console.log(`Error: ${e.message}`));
+  } else if (parsedFile.ext == ".md") {
+    await renameFile(process.argv[2], `${parsedFile.dir}/${newFileName}.md`)
+      .then(() => console.log("Markdown renamed successfully!"))
+      .catch((e) => console.log(`Error: ${e.message}`));
+  }
 }
 
 main().catch((e) => console.log(`Error: ${e.message}`));
